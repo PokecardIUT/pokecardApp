2e0006b6a0483da12709067050dcb7bb1fdc9780
theo <theo.tanchoux@etu.univ-lyon1.fr>
2019-02-13 14:13:49 +0100

    [FEAT] CardList to MVVM

--- app/src/main/java/com/example/lpiem/pokecardapp/presentation/presenter/factory/ViewModelFactory.kt
+++ app/src/main/java/com/example/lpiem/pokecardapp/presentation/presenter/factory/ViewModelFactory.kt
@@ -2,6 +2,7 @@ package com.example.lpiem.pokecardapp.presentation.presenter.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.lpiem.pokecardapp.presentation.presenter.CardListViewModel
 import com.example.lpiem.pokecardapp.presentation.presenter.LoginViewModel
 import com.example.lpiem.pokecardapp.presentation.presenter.SetsListViewModel
 
@@ -12,6 +13,9 @@ class ViewModelFactory: ViewModelProvider.Factory {
         }
         if(modelClass.isAssignableFrom(SetsListViewModel::class.java)){
             return SetsListViewModel() as T
+        }
+        if(modelClass.isAssignableFrom(CardListViewModel::class.java)){
+            return CardListViewModel() as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
